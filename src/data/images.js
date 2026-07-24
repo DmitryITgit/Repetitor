1e3e8d6f3f2eb574669ba2aec9bfc668ed274e3a
Дмитрий <dimanv2002@bk.ru>
2026-07-24 14:34:29 +0400

    fgdsg

--- src/data/images.js
+++ src/data/images.js
@@ -1,12 +1,6 @@
 import teacherImg from '../assets/images/teacher.jpg';
 import aboutImg from '../assets/images/about.jpg';
-import gallery1 from '../assets/images/gallery-1.jpg';
-import gallery2 from '../assets/images/gallery-2.jpg';
-import gallery3 from '../assets/images/gallery-3.jpg';
-import gallery4 from '../assets/images/gallery-4.jpg';
-import gallery5 from '../assets/images/gallery-5.jpg';
-import gallery6 from '../assets/images/gallery-6.jpg';
-import cert1 from '../assets/images/sert-1.jpg';
+import cert1 from '../assets/images/cert-1.jpg';
 import cert2 from '../assets/images/cert-2.jpg';
 import cert3 from '../assets/images/cert-3.jpg';
 import cert4 from '../assets/images/cert-4.jpg';
@@ -18,7 +12,6 @@ import student4 from '../assets/images/student-4.jpg';
 export const images = {
   teacher: teacherImg,
   about: aboutImg,
-  gallery: [gallery1, gallery2, gallery3, gallery4, gallery5, gallery6],
-  certificates: [sert1, cert2, cert3, cert4],
+  certificates: [cert1, cert2, cert3, cert4],
   students: [student1, student2, student3, student4],
 };
